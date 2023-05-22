c7df769725b42a2a3dfa2a64065a4cbaea016251
Salomão Pereira <salomao.psa3@gmail.com>
2023-05-21 23:17:27 -0300

    inclusão de mais explicações sobre os comandos GIT

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function mostrarInformacao(numero) {
     // Oculta todas as informações
-
     var infos = document.getElementsByClassName("info");
     //enquanto o valor de i for menor que o comprimento da coleção infos.
     // i++ incrementa o valor de i em 1 a cada iteração do loop.
